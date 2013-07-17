d126b42afa2cc6ca2562e4002f6d488aa8df8b88
Pierre <pierre@siliconstudio.co.jp>
2013-07-17 10:34:58 +0900

    [WinRT] Add BindingFlags file

---
+++ src/framework/WinRT/BindingFlags.cs
@@ -0,0 +1,17 @@
+﻿#if WinRT
+
+namespace System.Reflection
+{
+    [Flags]
+    public enum BindingFlags
+    {
+        Default = 0,
+        DeclaredOnly = 2,
+        Instance = 4,
+        Static = 8,
+        Public = 0x10,
+        NonPublic = 0x20,
+    }
+}
+
+#endif
