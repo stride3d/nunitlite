557e4d1c0b5337b2bd92ef9be4d8b3ba05346513
Pierre <pierre@siliconstudio.co.jp>
2013-07-17 10:09:00 +0900

    [WinRT] Add missing fields Path.AltDirectorySeparatorChar and Path.DirectorySeparatorChar

---
+++ src/framework/WinRT/PathExtension.cs
@@ -0,0 +1,12 @@
+﻿#if WinRT
+
+namespace NUnit.Framework 
+{
+    public static class Path
+    {
+        public static readonly char AltDirectorySeparatorChar = '/';
+        public static readonly char DirectorySeparatorChar = '\\';
+    }
+}
+
+#endif
